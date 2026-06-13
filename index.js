064a252ed92cf8630f32f7d8fd4d2aa42713943e
SuriyanRavi <suriyansm2104@gmail.com>
2026-06-14 00:01:49 +0530

    Updated Code

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
+ 
 function ageCalculate(){
     let input=document.getElementById("input").value;
     let Cal_btn=document.getElementById("calculateAge");
-    let result=document.getElementById("output");
+    let result=document.getElementById("output"); 
 
     let currentDate=new Date();
-    let birthdate=new Date(input);
-
+    let birthdate=new Date(input);//converts userinputdate od string into real date object
     let age=currentDate.getFullYear()-birthdate.getFullYear();
     let month=currentDate.getMonth()-birthdate.getMonth();
-    
 
+    if (input === "") {
+       alert("Enter Your age");
+    }else{
+        result.innerText=`Your Age is:${age}`
+    }
 
 }
+
